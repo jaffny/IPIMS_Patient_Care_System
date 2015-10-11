21b5fc237206a0e6341eb04ef0f58f0980efc777
Wadek <wkariniemi@gmail.com>
2015-10-11 03:53:46 -0700

    added tests.py

---
+++ IPIMS_Django_Source_Files/ipcms/ipcms/tests.py
@@ -0,0 +1,10 @@
+#to execute test, use 'python tests.py'
+
+import unittest
+
+class UrlsTest(unittest.TestCase):
+    def test_one_plus_one(self):
+        assert 1+1==2
+
+if __name__ == '__main__':
+    unittest.main()
